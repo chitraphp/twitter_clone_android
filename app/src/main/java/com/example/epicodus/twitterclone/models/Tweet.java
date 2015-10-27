5916a82da56383ab7284b682621deb9b460e6e67
chitra & Archana Bongale <student@epicodus.com>
2015-10-27 15:09:32 -0700

    Add finished project

---
+++ app/src/main/java/com/example/epicodus/twitterclone/models/Tweet.java
@@ -0,0 +1,77 @@
+package com.example.epicodus.twitterclone.models;
+
+import android.content.Context;
+
+import com.activeandroid.Model;
+import com.activeandroid.annotation.Column;
+import com.activeandroid.annotation.Table;
+import com.activeandroid.query.Select;
+import com.example.epicodus.twitterclone.R;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.TimeZone;
+
+/**
+ * Created by Guest on 10/27/15.
+ */
+@Table(name = "Tweets", id = "_id")
+public class Tweet extends Model {
+
+    @Column(name = "Content")
+    private String mContent;
+
+    @Column(name = "CreatedAt")
+    private long mCreatedAt;
+
+    @Column(name = "User")
+    private  User mUser;
+
+    public Tweet()
+    {
+        super();
+    }
+
+
+
+    public Tweet(String content, User user) {
+        mContent = content;
+        mUser = user;
+        mCreatedAt = new Date().getTime();
+    }
+    public String getContent() {
+        return mContent;
+    }
+
+    public void setContent(String content) {
+        mContent = content;
+    }
+
+    public long getCreatedAt() {
+        return mCreatedAt;
+    }
+
+    public void setCreatedAt(long createdAt) {
+        mCreatedAt = createdAt;
+    }
+
+    public User getUser() {
+        return mUser;
+    }
+
+    public void setUser(User user) {
+        mUser = user;
+    }
+
+    public String getFormattedTime(Context context) {
+        SimpleDateFormat formatter = new SimpleDateFormat(context.getString(R.string.formatted_time));
+        formatter.setTimeZone(TimeZone.getTimeZone(context.getString(R.string.timezone)));
+        return formatter.format(mCreatedAt);
+    }
+
+
+    public static List<Tweet> all() {
+        return new Select().from(Tweet.class).execute();
+    }
+}
